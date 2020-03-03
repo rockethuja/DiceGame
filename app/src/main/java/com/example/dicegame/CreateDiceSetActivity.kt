69d9e1c6d8ac41f202e36dc7873aafe0aebc269e
mbodenstein <milena.bodenstein@cleverbridge.com>
2020-03-03 10:41:19 +0100

    create empty activities and dice data class

---
+++ app/src/main/java/com/example/dicegame/CreateDiceSetActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dicegame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateDiceSetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_dice_set)
+    }
+}
