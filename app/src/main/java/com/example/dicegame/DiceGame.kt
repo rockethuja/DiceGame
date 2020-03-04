e6cca8eb753e28a0f60bbb8f8f947cdcac9932fc
julia <julia.holzmann93@gmx.de>
2020-03-04 10:11:32 +0100

    dice-item plus layout

--- app/src/main/java/com/example/dicegame/DiceGame.kt
+++ app/src/main/java/com/example/dicegame/DiceGame.kt
@@ -1,5 +1,8 @@
 package com.example.dicegame
 
+import android.graphics.Color.WHITE
+
+
 //data class DiceGame(val dices : List<Dice>)
 
-data class Dice(val min : Int, val max : Int, val colour : Int)
+data class Dice(val min : Int = 1, val max : Int = 6 , val colour : Int = WHITE)
--- app/src/main/java/com/example/dicegame/MainActivity.kt
+++ app/src/main/java/com/example/dicegame/MainActivity.kt
@@ -1,14 +1,31 @@
 package com.example.dicegame
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.FrameLayout
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.dice_item.*
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+
 }
+
+
+
+
+
 
 
