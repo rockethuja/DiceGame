71999c0969898f58c59c34204d385c885052b99b
julia <julia.holzmann93@gmx.de>
2020-03-04 11:20:28 +0100

    activity routing

--- app/src/main/java/com/example/dicegame/CreateDiceSetActivity.kt
+++ app/src/main/java/com/example/dicegame/CreateDiceSetActivity.kt
@@ -1,5 +1,6 @@
 package com.example.dicegame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,7 +10,7 @@ import kotlinx.android.synthetic.main.dice_item.*
 
 class CreateDiceSetActivity : AppCompatActivity() {
     val diceList = arrayListOf<Dice>()
-    lateinit var adapter : DiceAdapter
+    lateinit var adapter: DiceAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,15 +19,24 @@ class CreateDiceSetActivity : AppCompatActivity() {
         for (i in 0..9) {
             diceList.add(Dice())
         }
-        val adapter = DiceAdapter(this, diceList)
+        adapter = DiceAdapter(this, diceList)
         dice_list_view.adapter = adapter
     }
 
-  fun addDice(view: View){
+    fun addDice(view: View) {
         diceList.add(Dice())
         adapter.notifyDataSetChanged()
     }
 
+    fun startThrowDiceActivity(view: View) {
+        startActivity(
+            Intent(this, ThrowDiceActivity::class.java).apply {
+                putExtras(Bundle().apply {
+                    putParcelableArrayList("diceSet",diceList)
+                })
+            }
+        )
+    }
 
 
 }
--- app/src/main/java/com/example/dicegame/MainActivity.kt
+++ app/src/main/java/com/example/dicegame/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.dicegame
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,8 +15,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
+
+    fun startCreateDiceActivity(view : View){
+        val intent = Intent(this, CreateDiceSetActivity::class.java)
+        startActivity(intent)
+    }
+
 
 
 }
