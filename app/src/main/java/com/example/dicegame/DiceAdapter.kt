e42a32608c22a868f59edf20f3446ad00a100f2f
mbodenstein <milena.bodenstein@cleverbridge.com>
2020-03-05 09:25:37 +0100

    item selected spinner pre select von spinner oder so

--- app/src/main/java/com/example/dicegame/DiceAdapter.kt
+++ app/src/main/java/com/example/dicegame/DiceAdapter.kt
@@ -12,12 +12,11 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.FrameLayout
 import android.widget.Spinner
+import androidx.core.content.ContextCompat
 
 
-class DiceAdapter(private val mContext: Context, private val dices: ArrayList<Dice>) :
+class DiceAdapter(private val mContext: Context, val dices: ArrayList<Dice>) :
     BaseAdapter() {
-
-    // private val context : Context = mContext
 
     private val inflater: LayoutInflater =
         mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -42,15 +41,16 @@ class DiceAdapter(private val mContext: Context, private val dices: ArrayList<Di
             view = inflater.inflate(R.layout.dice_item, parent, false)
             holder = ViewHolder()
 
-            holder.dicePosition = view.findViewById(R.id.Position)
+            holder.dicePosition = view.findViewById(R.id.dicePosition)
             holder.increase = view.findViewById(R.id.increase)
             holder.reduce = view.findViewById(R.id.reduce)
             holder.spinner = view.findViewById(R.id.spinner)
-            holder.maxTextView = view.findViewById(R.id.textView_max)
+            holder.maxTextView = view.findViewById(R.id.maxTextView)
             holder.frameLayout = view.findViewById(R.id.frameLayout1)
             holder.deleteButton = view.findViewById(R.id.deleteButton)
 
             view.tag = holder
+
         } else {
             view = convertView
             holder = convertView.tag as ViewHolder
@@ -69,54 +69,57 @@ class DiceAdapter(private val mContext: Context, private val dices: ArrayList<Di
 
         dicePosition.text = position.toString()
         maxTextView.text = dice.max.toString()
-        setUpSpinner(spinner,frameLayout, position)
+        setUpSpinner(spinner, frameLayout, position)
 
         increase.setOnClickListener {
-            var amount = maxTextView.text.toString().toInt()
-            amount++
-            maxTextView.text = amount.toString()
+            //            var amount = dice.max//maxTextView.text.toString().toInt()
+//            amount++
+            dice.max++
+            maxTextView.text = dice.max.toString()
         }
 
         reduce.setOnClickListener {
-            var amount = maxTextView.text.toString().toInt()
-            amount--
-            maxTextView.text = amount.toString()
+            //            var amount = maxTextView.text.toString().toInt()
+//            amount--
+            dice.max--
+            maxTextView.text = dice.max.toString()
         }
 
-        deleteButton.setOnClickListener{
-            dices.remove(dices.get(position))
+        deleteButton.setOnClickListener {
+            dices.remove(dice)
+//            dices.remove(dices.get(position))
             this.notifyDataSetChanged()
         }
 
-
         return view
     }
-    fun addDice(){
+
+    fun addDice() {
         dices.add(Dice())
         this.notifyDataSetChanged()
     }
 
-    fun changeColor(color :String, frame :FrameLayout, position: Int){
-        frame.setBackgroundColor(Color.parseColor(color))
-        val dice : Dice = dices.get(position)
-        dice.colour = Color.parseColor(color)
+    fun changeColor(color: Int, frame: FrameLayout, position: Int) {
+        frame.setBackgroundColor(color)
+//        frame.setBackgroundColor(R.color.cyan)
+        val dice: Dice = dices[position] as Dice
+        dice.colour = color
     }
 
 
     private class ViewHolder {
-        lateinit var  dicePosition: TextView
+        lateinit var dicePosition: TextView
         lateinit var maxTextView: TextView
         lateinit var spinner: Spinner
         lateinit var increase: Button
         lateinit var reduce: Button
         lateinit var frameLayout: FrameLayout
-        lateinit var  deleteButton: Button
+        lateinit var deleteButton: Button
 
     }
 
     fun setUpSpinner(spinner: Spinner, frame: FrameLayout, dicePosition: Int) {
         //die position der farbe die der wüfel im spinner ausgewählt hat
-
 
 //        val spinner: Spinner = view.findViewById(R.id.spinner)
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
@@ -126,26 +129,44 @@ class DiceAdapter(private val mContext: Context, private val dices: ArrayList<Di
                 position: Int,
                 id: Long
             ) {
+                val cyan = ContextCompat.getColor(mContext, R.color.cyan)
+                val green = ContextCompat.getColor(mContext, R.color.green)
+                val blue = ContextCompat.getColor(mContext, R.color.blue)
+                val red = ContextCompat.getColor(mContext, R.color.red)
+                val yellow = ContextCompat.getColor(mContext, R.color.yellow)
+
+
                 val selectedItem = parent.getItemAtPosition(position).toString()
                 when (selectedItem) {
-                    "cyan" -> changeColor("#03FFEE", frame, dicePosition)
-                    "red" -> changeColor("#E80231", frame, dicePosition)
-                    "green" -> changeColor("#83E802" ,frame, dicePosition)
-                    "yellow"-> changeColor("#FFA40B", frame, dicePosition)
-                    "blue"-> changeColor("#1C03FF", frame, dicePosition)
+                    "cyan" -> changeColor(cyan, frame, dicePosition)
+                    "red" -> changeColor(red, frame, dicePosition)
+                    "green" -> changeColor(green, frame, dicePosition)
+                    "yellow" -> changeColor(yellow, frame, dicePosition)
+                    "blue" -> changeColor(blue, frame, dicePosition)
                 }
             }
 
-            override fun onNothingSelected(parent: AdapterView<*>) {
-            }
+            override fun onNothingSelected(parent: AdapterView<*>) {}
         }
+
         ArrayAdapter.createFromResource(
             mContext, R.array.colours,
             android.R.layout.simple_spinner_item
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinner.adapter = adapter
+            spinner.setSelection(getColourPosition(dices[dicePosition].colour))
         }
+    }
+
+    fun getColourPosition(colour: Int) : Int {
+        val colours =  mContext.resources.getStringArray(R.array.colours);
+
+        for (i in colours.indices) {
+            if (Color.parseColor(colours[i]) == colour)
+                return i
+        }
+        return -1
     }
 
 }
